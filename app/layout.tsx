4ef6419aced384cf37c85c09045a162929464266
hamza-ouaddi <hamza0addi@gmail.com>
2023-12-05 16:03:14 +0100

    style: configurate fonts and brand colors on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next/types";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "TechHub",
+  description:
+    "A platform for all things tech and development. TechHub is a vibrant community where enthusiasts, developers, and learners converge to ask questions, share insights, and collaborate. Explore the ever-evolving world of technology, connect with like-minded individuals, and elevate your coding journey. TechHub - Where Tech Meets Community",
+  icons: {
+    icon: "/assets/images/techhub-logo-icon.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
